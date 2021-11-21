e02114d3ce32b786a4a6ebdeec227c1a16c35609
owermaiden <ower.maiden@cybertekschool.com>
2021-11-21 23:47:06 +0300

    IsPalindrome

--- ReplitTask-Palindrome/src/Palindrome.java
+++ ReplitTask-Palindrome/src/Palindrome.java
@@ -1,10 +1,12 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String word = "Noon";
+        String word = "Race car";
         System.out.println(isPalindrome(word));
     }
     public static boolean isPalindrome(String word){
+
         String str = word.toLowerCase();
+        str = str.replace(" ", "");
         for(int i=0; i < str.length()/2; i++) {
 
             if(str.charAt(i) != str.charAt(str.length()-1 - i)) {
