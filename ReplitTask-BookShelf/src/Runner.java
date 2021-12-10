6b6685d7652107c10d7b0f2fe65f4caca9439253
owermaiden <ower.maiden@cybertekschool.com>
2021-12-10 17:28:44 +0300

    Book Shelf

---
+++ ReplitTask-BookShelf/src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.setAuthor("ali veli");
+        book.setTitle("The, how are you to the");
+        System.out.println(book.getAuthor());
+        System.out.println(book.getTitle());
+    }
+}
